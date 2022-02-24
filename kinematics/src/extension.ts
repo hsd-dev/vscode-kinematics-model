d1b4eb9ec7ff73a3c48916ceacb50420c6afd2b2
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2022-02-24 09:09:03 +0100

    created a basic WebGL based preview window

--- kinematics/src/extension.ts
+++ kinematics/src/extension.ts
@@ -29,6 +29,54 @@ function activate(context) {
     // Push the disposable to the context's subscriptions so that the 
     // client can be deactivated on extension deactivation
     context.subscriptions.push(disposable);
+
+    // related to URDF preview
+    const disposableSidePreview = vscode.commands.registerCommand('kinematics.openPreview', () => {
+        initKinematicsPreview(context);
+    });
+
+    context.subscriptions.push(disposableSidePreview);
+
+    function initKinematicsPreview(context) {
+       // Create and show a new webview
+      const panel = vscode.window.createWebviewPanel(
+        // Webview id
+        'kinematics.preview',
+        // Webview title
+        'Kinematics Preview',
+        // This will open the second column for preview inside editor
+        vscode.ViewColumn.Beside,
+        {
+            // Enable scripts in the webview
+            enableScripts: true
+        }
+      );
+      panel.webview.html = getWebviewContent(context, panel);
+    }
+}
+
+function getWebviewContent(context, panel) {
+  const threejs =  vscode.Uri.file(context.extensionPath+"/js/three.js");  
+  const threejsUri = panel.webview.asWebviewUri(threejs);
+  const geometryjs =  vscode.Uri.file(context.extensionPath+"/js/geometry.js");  
+  const geometryjsUri = panel.webview.asWebviewUri(geometryjs);
+
+  return `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>My first three.js app</title>
+      <style>
+        body { margin: 0; }
+      </style>
+    </head>
+    <body>
+      <script src=${threejsUri}></script>
+      <script src=${geometryjsUri}></script>
+    </body>
+  </html>
+  `
 }
 
 exports.activate = activate;
