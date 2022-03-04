3a1a281d0ce96e20db53f33aa2ce891482ce249d
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2022-03-04 12:14:05 +0100

    visualize ros3djs scene

--- kinematics/lib/extension.js
+++ kinematics/lib/extension.js
@@ -1,4 +1,13 @@
 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const vscode = require("vscode");
@@ -44,24 +53,65 @@ function activate(context) {
             enableScripts: true
         });
         panel.webview.html = getWebviewContent(context, panel);
+        setActiveEditorContent(panel);
     }
-}
-function getWebviewContent(context, panel) {
-    return `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>My first three.js app</title>
-      <style>
-        body { margin: 0; }
-      </style>
-    </head>
-    <body>
-      <p>Hi</p>
-    </body>
-  </html>
-  `;
+    function setActiveEditorContent(panel) {
+        var _a;
+        const document = (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document;
+        if (document) {
+            panel.webview.postMessage(loadModel(document));
+        }
+    }
+    function loadModel(document) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return { message: 'loadModel' };
+        });
+    }
+    function getWebviewContent(context, panel) {
+        const threejs = vscode.Uri.file(context.extensionPath + "/js/three.js");
+        const threejsUri = panel.webview.asWebviewUri(threejs);
+        const ros3d = vscode.Uri.file(context.extensionPath + "/src/ros3djs/build/ros3d.js");
+        const ros3dUri = panel.webview.asWebviewUri(ros3d);
+        const roslib = vscode.Uri.file(context.extensionPath + "/node_modules/roslib/build/roslib.js");
+        const roslibUri = panel.webview.asWebviewUri(roslib);
+        return `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>My first three.js app</title>
+          <style>
+            body { margin: 0; }
+          </style>
+          <script src=${threejsUri}></script>
+          <script src=${roslibUri}></script>
+          <script src=${ros3dUri}></script>
+          <script>
+            window.onload = init;
+
+            var global = this;
+            var viewer = undefined;
+
+            function init() {
+              this.viewer = new ROS3D.Viewer({
+                divID : 'urdf',
+                width : 800,
+                height : 600,
+                antialias : true
+              });
+
+              // Add a grid.
+              this.viewer.addObject(new ROS3D.Grid());
+            }
+          </script>
+        </head>
+        <body>
+          <h2>URDF Renderer</h2>
+        </body>
+        <div id="urdf"></div>
+      </html>
+      `;
+    }
 }
 exports.activate = activate;
 //# sourceMappingURL=extension.js.map
--- kinematics/src/extension.ts
+++ kinematics/src/extension.ts
@@ -4,9 +4,6 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import { LanguageClient } from 'vscode-languageclient/node';
 import { spawn } from 'child_process';
-import * as antlr4ts from 'antlr4ts';
-import { DebugInternalKinematicsLexer } from './parser/DebugInternalKinematicsLexer';
-import { DebugInternalKinematicsParser } from './parser/DebugInternalKinematicsParser';
 
 
 function activate(context: vscode.ExtensionContext) {
@@ -15,10 +12,10 @@ function activate(context: vscode.ExtensionContext) {
         var jar = context.asAbsolutePath(path.join('resources', 'de.fraunhofer.ipa.kinematics.xtext.ide-1.0.0-SNAPSHOT-ls.jar'));
         var child = spawn('java', ['-Xdebug', '-Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y', '-jar', jar, '-log debug']);
         console.log(child.stdout.toString());
-        child.stdout.on('data', function (chunk: any) {
+        child.stdout.on('data', function (chunk) {
             console.log(chunk.toString());
         });
-        child.stderr.on('data', function (chunk: any) {
+        child.stderr.on('data', function (chunk) {
             console.error(chunk.toString());
         });
         return Promise.resolve(child);
@@ -54,42 +51,66 @@ function activate(context: vscode.ExtensionContext) {
         }
       );
       panel.webview.html = getWebviewContent(context, panel);
-      getModel();
+      setActiveEditorContent(panel);
     }
-}
 
-function getWebviewContent(context: vscode.ExtensionContext, panel: vscode.WebviewPanel) {
-  return `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>My first three.js app</title>
-      <style>
-        body { margin: 0; }
-      </style>
-    </head>
-    <body>
-      <p>Hi</p>
-    </body>
-  </html>
-  `
-}
+    function setActiveEditorContent(panel: vscode.WebviewPanel) {
+      const document = vscode.window.activeTextEditor?.document;
+      if (document) {
+        panel.webview.postMessage(loadModel(document));
+      }
+    }
 
-async function getModel() {
-  const document = vscode.window.activeTextEditor?.document;
-  if (document) {
-    console.log(document.getText())
+    async function loadModel(document: vscode.TextDocument) {
+      return {message: 'loadModel'};
+    }
 
-    // Create the lexer and parser
-    let inputStream = antlr4ts.CharStreams.fromString(document.getText());
-    console.log(inputStream)
-    let lexer = new DebugInternalKinematicsLexer(inputStream);
-    let tokenStream = new antlr4ts.CommonTokenStream(lexer);
-    let parser = new DebugInternalKinematicsParser(tokenStream);
-    let parse_tree = parser.ruleRobot()
-    console.log(parse_tree.getChild)
-  }
+    function getWebviewContent(context: vscode.ExtensionContext, panel: vscode.WebviewPanel) {
+      const threejs =  vscode.Uri.file(context.extensionPath+"/js/three.js");
+      const threejsUri = panel.webview.asWebviewUri(threejs);
+      const ros3d =  vscode.Uri.file(context.extensionPath+"/src/ros3djs/build/ros3d.js");
+      const ros3dUri = panel.webview.asWebviewUri(ros3d);
+      const roslib =  vscode.Uri.file(context.extensionPath+"/node_modules/roslib/build/roslib.js");
+      const roslibUri = panel.webview.asWebviewUri(roslib);
+
+      return `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>My first three.js app</title>
+          <style>
+            body { margin: 0; }
+          </style>
+          <script src=${threejsUri}></script>
+          <script src=${roslibUri}></script>
+          <script src=${ros3dUri}></script>
+          <script>
+            window.onload = init;
+
+            var global = this;
+            var viewer = undefined;
+
+            function init() {
+              this.viewer = new ROS3D.Viewer({
+                divID : 'urdf',
+                width : 800,
+                height : 600,
+                antialias : true
+              });
+
+              // Add a grid.
+              this.viewer.addObject(new ROS3D.Grid());
+            }
+          </script>
+        </head>
+        <body>
+          <h2>URDF Renderer</h2>
+        </body>
+        <div id="urdf"></div>
+      </html>
+      `
+    }
 }
 
 exports.activate = activate;
